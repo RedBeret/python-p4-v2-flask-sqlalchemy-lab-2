bd30a1e17298c9aac613ed99e7390b247f7fe083
Steven Espinoza <espisteven5@gmail.com>
2024-01-12 11:14:49 -0800

    done

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, make_response, request
+from flask import Flask
 from flask_migrate import Migrate
-from models import Review, db
+from models import db
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
@@ -14,21 +14,6 @@ db.init_app(app)
 @app.route("/")
 def index():
     return "<h1>Flask SQLAlchemy Lab 2</h1>"
-
-
-@app.route("/reviews", methods=["GET", "POST"])
-def reviews():
-    if request.method == "GET":
-        return make_response([review.to_dict() for review in Review.query.all()], 200)
-    else:
-        comment = request.form.get("comment")
-        item_id = request.form.get("item_id")
-        customer_id = request.form.get("customer_id")
-
-        r = Review(comment=comment, item_id=item_id, customer_id=customer_id)
-        db.session.add(r)
-        db.session.commit()
-        return make_response(r.to_dict(), 201)
 
 
 if __name__ == "__main__":
--- server/models.py
+++ server/models.py
@@ -3,6 +3,7 @@ from sqlalchemy import MetaData
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
+
 metadata = MetaData(
     naming_convention={
         "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -18,12 +19,14 @@ class Customer(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
-    reviews = db.Relationship(
-        "Review", back_populates="customer", cascade="all, delete-orphan"
-    )
-    items = association_proxy("reviews", "item")
+    serialize_rules = ("-reviews.customer",)
 
-    serialize_rules = ("-reviews.customer", "-reviews.item", "-reviews.customer_id")
+    reviews = db.relationship("Review", back_populates="customer")
+
+    # Association proxy to get items for this customer through reviews
+    items = association_proxy(
+        "reviews", "item", creator=lambda item_obj: Review(item=item_obj)
+    )
 
     def __repr__(self):
         return f"<Customer {self.id}, {self.name}>"
@@ -33,15 +36,12 @@ class Item(db.Model, SerializerMixin):
     __tablename__ = "items"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True)
+    name = db.Column(db.String)
     price = db.Column(db.Float)
 
-    reviews = db.Relationship(
-        "Review", back_populates="item", cascade="all, delete-orphan"
-    )
-    customers = association_proxy("reviews", "customer")
-
     serialize_rules = ("-reviews.item",)
+
+    reviews = db.relationship("Review", back_populates="item")
 
     def __repr__(self):
         return f"<Item {self.id}, {self.name}, {self.price}>"
@@ -52,10 +52,14 @@ class Review(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     comment = db.Column(db.String)
+
+    serialize_rules = ("-customer.reviews", "-item.reviews")
+
     customer_id = db.Column(db.Integer, db.ForeignKey("customers.id"))
     item_id = db.Column(db.Integer, db.ForeignKey("items.id"))
 
-    customer = db.Relationship("Customer", back_populates="reviews")
-    item = db.Relationship("Item", back_populates="reviews")
+    customer = db.relationship("Customer", back_populates="reviews")
+    item = db.relationship("Item", back_populates="reviews")
 
-    serialize_rules = ("-customer.reviews", "-item.reviews")
+    def __repr__(self):
+        return f"<Review {self.id}, {self.comment}, {self.customer.name}, {self.item.name}>"
